a74141a861f97865a499961319c531ff26c6e870
kennychaos7 <kennychaos7@gmail.com>
2018-11-14 16:36:04 +0800

    backup 0.2

--- app/src/main/jni/org_k_JNIUtils.cpp
+++ app/src/main/jni/org_k_JNIUtils.cpp
@@ -98,16 +98,17 @@ int K::map_decompress(jbyte *compress,jbyte *uncompress, int len) {
 }
 
 
-void K::analysis(JNIEnv *env, jbyteArray in,int32_t *point_pixels){
+void K::analysis(JNIEnv *env, jbyteArray in,int32_t *point_pixels) {
     jbyte *bp_in = env->GetByteArrayElements(in, 0);
     /*
      * 协议相关
      */
     uint8_t map_type = bp_in[0];
     uint8_t nouse = bp_in[1];
-    uint8_t update_num = toUINT16(bp_in[2],bp_in[3]);
+    uint8_t update_num = toUINT16(bp_in[2], bp_in[3]);
     int interval = 0;
-    int INDEX = 0;
+    int x_begin = 0;
+    int y_begin = 0;
     for (int i = 0; i < 100; ++i) {
 
         uint16_t block_id = toUINT16(bp_in[4 + interval], bp_in[5 + interval]);
@@ -119,7 +120,13 @@ void K::analysis(JNIEnv *env, jbyteArray in,int32_t *point_pixels){
         int red = 0;
         int green = 0;
         int blue = 0;
+
         if (data_size > 0) {
+            x_begin = (block_id - 1) % 10 * 100;
+            y_begin = (block_id - 1) / 10 * 100;
+            LOGI("x_begin = %d, y_begin = %d, index = %d", x_begin, y_begin,
+                 x_begin + y_begin * 100);
+
             jbyteArray compress_buf = env->NewByteArray(data_size);
             jbyte *p_compress = env->GetByteArrayElements(compress_buf, 0);
             env->GetByteArrayRegion(in, 10 + interval, data_size, p_compress); // 复制数据
@@ -129,15 +136,26 @@ void K::analysis(JNIEnv *env, jbyteArray in,int32_t *point_pixels){
 
             map_decompress(p_compress, p_uncompress, data_size); // 解压数据
 
+            int x = 0;
+            int y = 0;
             for (int j = 0; j < 2500; ++j) {
+                if (j != 0 && j % 25 == 0) {
+                    y++;
+                    x = 0;
+//                    LOGI("y = %d", (y + y_begin) * 1000);
+                }
                 auto *point_type = new int();
                 this->ToTYPE((const uint8_t) p_compress[j], point_type);
                 for (int q = 0; q < 4; ++q) {
                     if (point_type[q] == TYPE_BLOCK) {
+                        LOGI("index = %d",x + x_begin + (y + y_begin) * 1000);
+                        red = 255;
+                        green = 255;
                         blue = 255;
+                        drawPoint(point_pixels, x + x_begin + (y + y_begin) * 1000, alpha, red, green,
+                                  blue);
                     }
-                    drawPoint(point_pixels,INDEX,alpha,red,green,blue);
-                    INDEX ++ ;
+                    x++;
                 }
                 free(point_type);
             }
@@ -145,20 +163,29 @@ void K::analysis(JNIEnv *env, jbyteArray in,int32_t *point_pixels){
             env->ReleaseByteArrayElements(compress_buf, p_compress, 0);
             env->ReleaseByteArrayElements(uncompress_buf, p_uncompress, 0);
 
-        } else{
-            for (int d = 0; d < 2500 * 4; ++d) {
-                red = 112;
-                green = 128;
-                blue = 144;
-                drawPoint(point_pixels,INDEX,alpha,red,green,blue);
-                INDEX ++ ;
-            }
         }
-
+//        else {
+//            int x = 0;
+//            int y = 0;
+//            for (int j = 0; j < 2500; ++j) {
+//                if (j != 0 && j % 25 == 0) {
+//                    y++;
+//                    x = 0;
+//                    LOGI("y = %d", (y + y_begin) * 1000);
+//                }
+//                for (int q = 0; q < 4; ++q) {
+//                    red = 255;
+//                    green = 255;
+//                    blue = 255;
+//                    drawPoint(point_pixels, x + x_begin + (y + y_begin) * 1000, alpha, red, green,
+//                              blue);
+//                    x++;
+//                }
+//            }
+//        }
         interval = data_size + 6 + interval;
 
     }
-    LOGI("INDEX = %d",INDEX);
     env->ReleaseByteArrayElements(in, bp_in, 0);
 }
 
